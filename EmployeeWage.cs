5e72a9fbfce70ad33cc584f8810e0e124db8142e
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-21 16:25:05 +0530

    ADDED:partTime empHours to calculate empwage on basis of Full and part time

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -6,21 +6,25 @@ namespace EmployeeWage
 {
     class EmployeeWage
     {
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
         int DAILY_WAGE_PERHOUR = 20;
-        int EMP_HOURS = 8;
+        int FULL_TIME_EMP_HOURS = 8;
+        int PART_TIME_EMP_HOURS = 4;
         int ABSENT_EMP_HOURS = 0;
       public int CheckEmployee()
         {
             Random random = new Random();
-            int employeeCheck = random.Next(0, 2);
+            int employeeCheck = random.Next(0, 3);
             return employeeCheck;
         }
        public int CalculateWage()
         {
             int employeeCheck = CheckEmployee();
-            return employeeCheck == IS_PRESENT ? DAILY_WAGE_PERHOUR * EMP_HOURS :
-                                                DAILY_WAGE_PERHOUR * ABSENT_EMP_HOURS;
+            return employeeCheck == IS_FULL_TIME ? DAILY_WAGE_PERHOUR * FULL_TIME_EMP_HOURS :
+                   employeeCheck == IS_PART_TIME ? DAILY_WAGE_PERHOUR * PART_TIME_EMP_HOURS :
+                                     DAILY_WAGE_PERHOUR * ABSENT_EMP_HOURS;
         }
+
     }
 }
