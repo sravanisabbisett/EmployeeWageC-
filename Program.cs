e0bfbd922c64c32ef464ec170037d25afba816dd
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-21 15:53:47 +0530

    STARTED:employeewage computation

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employeeWage");
+            Console.ReadKey();
+        }
+    }
+}
